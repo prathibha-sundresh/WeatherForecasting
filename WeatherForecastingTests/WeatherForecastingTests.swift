691efc0475f53b633e5dded191b6c71a5303f4d3
prathiba-sundresh <ppprathu6@gmail.com>
2020-07-09 17:32:03 +0530

    Initial Commit

---
+++ WeatherForecastingTests/WeatherForecastingTests.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherForecastingTests.swift
+//  WeatherForecastingTests
+//
+//  Created by prathibha sundresh on 09/07/20.
+//  Copyright © 2020 prathibha sundresh. All rights reserved.
+//
+
+import XCTest
+@testable import WeatherForecasting
+
+class WeatherForecastingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
